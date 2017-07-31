ec387e028bdd7b999d9bc8093e3bfef6b08b76ba
Perry Fustero <pjfust@gmail.com>
2017-07-31 09:51:46 -0400

    finish section 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/posts' => 'posts#index'
+  get '/posts/new' => 'posts#new'
+  get '/posts/:id' => 'posts#show'
+  post '/posts' => 'posts#create'
 end
